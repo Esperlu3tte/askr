0d7b6a04c346d0b9bf9f0197e4352e13ec9537c8
Esperlu&te <29789231+Esperlu3tte@users.noreply.github.com>
2022-09-10 23:55:54 +0200

    Remove base64 from svg endpoint + cache header

--- routes.js
+++ routes.js
@@ -14,27 +14,21 @@ function unprocessableEntityError(res) {
   return res.end();
 }
 
-router.on("GET", "/svg", (req, res, _params, _store, { src, text, size, color }) => {
-  let svg;
-  if (src) {
-    if (!validator.isBase64(src)) {
-      return unprocessableEntityError(res);
-    }
-    svg = fromBase64Source(src);
-  } else if (text) {
-    if (size && !validator.isInt(size, { min: 1, max: 2000 })
-      || color && !validator.isHexColor(color)
-    ) {
-      return unprocessableEntityError(res);
-    }
-    svg = fromText(text, size, color ? color[0] === "#" ? color : `#${color}` : undefined);
+router.on("GET", "/svg", (req, res, _params, _store, { text, size, color }) => {
+  if (!text
+    || size && !validator.isInt(size, { min: 1, max: 2000 })
+    || color && !validator.isHexColor(color)
+  ) {
+    return unprocessableEntityError(res);
   }
 
+  const svg = fromText(text, size, color ? color[0] === "#" ? color : `#${color}` : undefined);
   if (!svg) {
     return unprocessableEntityError(res);
   }
 
-  res.setHeader('Content-Type', "image/svg+xml");
+  res.setHeader("Content-Type", "image/svg+xml");
+  res.setHeader("Cache-Control", `max-age=${7 * 24 * 60 * 60}, stale-while-revalidate=${1 * 24 * 60 * 60}`);
   return res.end(svg);
 });
 
